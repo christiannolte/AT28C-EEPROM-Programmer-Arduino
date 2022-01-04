1c3c8efb73883c2c6bbc13070692f0a38bfb1364
Christian Nolte <christiannolte@users.noreply.github.com>
2022-01-04 20:09:40 +0100

    Update at28c_programmer_firmware.ino

--- at28c_programmer_firmware/at28c_programmer_firmware.ino
+++ at28c_programmer_firmware/at28c_programmer_firmware.ino
@@ -32,10 +32,10 @@
 #define OUTPUT_ENABLE 8
 #define WRITE_ENABLE  9
 
-// I/O pins
+// I/O pins      lsb                         msb
 const int I[] = {46, 47, 48, 49, 50, 51, 52, 53};
 
-// Address pins
+// Address pins  lsb                                                     msb
 const int A[] = {26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40};
 
 // Input buffers
